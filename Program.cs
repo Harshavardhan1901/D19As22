fe73b823f7cfb8baaed8ab86048b313b7d24331a
harsha1901@yahoo.com <harsha1901@yahoo.com>
2023-08-03 14:41:50 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+
+namespace Assignment22
+{
+    internal class Program
+    {
+        public static void MapProperties(Source source, Destination destination)
+        {
+            Type sourceType = source.GetType();
+            Type destinationType = destination.GetType();
+
+            PropertyInfo[] sourceProperties = sourceType.GetProperties();
+            PropertyInfo[] destinationProperties = destinationType.GetProperties();
+
+            foreach (var sourceProperty in sourceProperties)
+            {
+                PropertyInfo destinationProperty = Array.Find(destinationProperties, p => p.Name == sourceProperty.Name);
+                if (destinationProperty != null && destinationProperty.PropertyType == sourceProperty.PropertyType)
+                {
+                    destinationProperty.SetValue(destination, sourceProperty.GetValue(source));
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Source source = new Source
+            {
+                Id = 65,
+                Name = "James Cameron",
+                Age = 30,
+                Address = "1-2-3,River Street,Los Angels"
+            };
+
+            Destination destination = new Destination();
+            MapProperties(source, destination);
+
+            Console.WriteLine("Destination properties after mapping:");
+            Console.WriteLine($"Id: {destination.Id}");
+            Console.WriteLine($"Name: {destination.Name}");
+            Console.WriteLine($"Age: {destination.Age}");
+            Console.WriteLine($"City: {destination.City}"); // Should be empty as it doesn't exist in Source
+            Console.WriteLine($"Country: {destination.Country}"); // Should be empty as it doesn't exist in Source
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Source.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Assignment22
+{
+    public class Source
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class Destination
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
